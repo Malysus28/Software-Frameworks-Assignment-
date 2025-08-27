7d1beae5c1ebda448b8001153806b94b65598b49
MaleesA28 <mariyammalys@gmail.com>
2025-08-27 16:30:25 +1000

    Add chats/channels pages, navbar updates, home tweaks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,29 @@ import { Routes } from '@angular/router';
 import { Home } from '../home/home';
 import { Profile } from '../profile/profile';
 import { Login } from '../login/login';
+import { Chats } from '../chats/chats';
+import { Channels } from '../channels/channels';
 import { authGuard } from './services/auth/auth-guard';
 
 // defining the routes for website
 export const routes: Routes = [
+  // home
   { path: '', component: Home, title: 'Home' },
+  // profile
   {
     path: 'profile',
     component: Profile,
     title: 'Profile',
     canActivate: [authGuard],
   },
+  // login
   { path: 'login', component: Login, title: 'Login' },
+  // chat
+  { path: 'chat', component: Chats, title: 'Chat' },
+  // channels
+  {
+    path: 'channels',
+    component: Channels,
+    title: 'Channels',
+  },
 ];
